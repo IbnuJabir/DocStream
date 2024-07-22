dc7b9180d64fb36efc2d3e26edbb3d12c2fab4a2
IbnuJabir <kedirjabir12@gmail.com>
2024-07-22 15:26:43 +0300

    cew front added to cors origin

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(
       "https://docstream-client.onrender.com",
       "http://localhost:3000",
       "http://localhost:3001",
-      "https://docstreamfront.netlify.app/appointment"
+      "https://docstreamfront.netlify.app"
     ],
     methods: ["POST", "GET", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
